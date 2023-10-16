157bffb6452561cbaa9298c4d44b233d72eee0ab
RyanYu <ryan.yu@riversoft.com.tw>
2023-10-16 11:44:42 +0800

    style: 💄 styled css

--- src/App.vue
+++ src/App.vue
@@ -61,9 +61,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <ul class="w-full h-screen overflow-auto bg-gradient-to-l from-pink-300 to-green-300 py-4">
-    <li v-for="item in copyList" :key="item.id" @mouseover="editId = item.id" @mouseleave="editId = ''" class="relative px-4 py-2">
-      <p class="line-clamp-1 text-sm text-#333 fw-700">{{ item.label }}</p>
+  <ul class="w-full h-screen overflow-auto background-gradient pb-4 backdrop-blur-md flex flex-col gap-y-1px">
+    <li v-for="item in copyList" :key="item.id" @mouseover="editId = item.id" @mouseleave="editId = ''" class="relative px-4 py-2 border border-black">
+      <p class="line-clamp-1 text-sm fw-700 text-stroke-black">{{ item.label }}</p>
 
       <button v-show="editId === item.id" @click="handleCopy(item.label)" class="absolute w-1/2 h-full top-0 left-0 bg-white/25 hover:bg-white/50 backdrop-blur-sm flex items-center justify-center">
         <img src="./assets/copy.svg" alt="Copy the text" class="w-5 h-5">
@@ -73,9 +73,9 @@ onMounted(() => {
       </button>
 
     </li>
-    <li v-show="copyList.length === 0 && !isAdding" class="text-white fw-700 text-center">目前沒有資料唷</li>
+    <li v-show="copyList.length === 0 && !isAdding" class="text-white fw-700 text-center mt-30">目前沒有資料唷</li>
 
-    <li v-show="isAdding" class="px-4">
+    <li v-show="isAdding" class="px-4 mt-2">
       <div class="rounded-10px overflow-hidden relative w-full h-9">
         <div class="absolute top-0 left-0 w-full h-full border-gradient"></div>
         <input v-model="textModel" @keyup.enter="handleAddText" type="text" class="absolute w-[calc(100%-4px)] left-2px h-[calc(100%-4px)] top-2px rounded-8px px-2 py-1 focus:outline-none text-sm text-stone-500">
@@ -104,12 +104,31 @@ onMounted(() => {
 
 *::-webkit-scrollbar-thumb {
   border-radius: 4px;
-  background-color: rgb(183, 255, 74);
+  background-color: #b7ff4a;
   border: 1px solid slategrey;
 }
 
 *::-webkit-scrollbar-track {
   box-shadow: transparent;
+}
+
+.bg-gradient-animation {
+  animation: gradient 1s infinite linear;
+}
+
+.background-gradient {
+	background: linear-gradient(0deg, #4ccaf7, #23d5ab, #23d5ab, #4ccaf7);
+	background-size: 400% 400%;
+	animation: backgroundgradient 4s infinite linear;
+}
+
+@keyframes backgroundgradient {
+  0% {
+    background-position: 0% 0%;
+  }
+  100% {
+    background-position: 0% 400%;
+  }
 }
 
 .border-gradient {
